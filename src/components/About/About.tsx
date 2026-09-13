0627b45728e5f2649b768b63f7e552bf0b3e1502
Shoha <shoha28kyu@gmail.com>
2026-09-13 17:11:55 +0200

    Added links in About section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,6 @@
 import { useTranslation } from "react-i18next";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { Download } from "lucide-react";
 import uzbekistanOutline from "../../assets/uzb.svg";
 import "./About.css";
 
@@ -11,6 +13,31 @@ export default function About() {
         <div className="about-text-col">
           <h2 className="about-title">{t("about.title")}</h2>
           <p className="about-text">{t("about.text")}</p>
+          <div className="about-actions">
+            <a href="/CV.pdf" download className="about-btn about-btn-primary">
+              <Download size={16} />
+              {t("about.downloadCV")}
+            </a>
+            <a
+              href="https://github.com/Shoha28"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="about-btn about-btn-secondary"
+            >
+              <FaGithub size={16} />
+              GitHub
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/shoha-kayumov/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="about-btn about-btn-secondary"
+            >
+              <FaLinkedin size={16} />
+              LinkedIn
+            </a>
+          </div>
         </div>
         <div className="about-map-col">
           <div className="about-map-wrapper">
