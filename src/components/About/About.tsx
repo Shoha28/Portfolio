7d7831c709a73b8f8baaa8dcb871f288838c0e06
Shoha <shoha28kyu@gmail.com>
2026-09-13 15:44:15 +0200

    Enhanced about section with a map, changes styles

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import uzbekistanOutline from "../../assets/uzb.svg";
 import "./About.css";
 
 export default function About() {
@@ -7,8 +8,21 @@ export default function About() {
   return (
     <section id="about" className="about-container">
       <div className="about-content">
-        <h2 className="about-title">{t("about.title")}</h2>
-        <p className="about-text">{t("about.text")}</p>
+        <div className="about-text-col">
+          <h2 className="about-title">{t("about.title")}</h2>
+          <p className="about-text">{t("about.text")}</p>
+        </div>
+        <div className="about-map-col">
+          <div className="about-map-wrapper">
+            <div
+              className="about-map"
+              style={{
+                WebkitMaskImage: `url(${uzbekistanOutline})`,
+                maskImage: `url(${uzbekistanOutline})`,
+              }}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
