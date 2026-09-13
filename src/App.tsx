19116cdeb64a5699b9f5e0529ff9ef36d9b63ca7
Shoha <shoha28kyu@gmail.com>
2026-09-13 11:35:31 +0200

    Initialization

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Header></Header>
+      <About></About>
+    </>
+  );
+}
